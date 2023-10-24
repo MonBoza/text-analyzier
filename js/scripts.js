d81714ce001afe7d37b2d7a05fe8cd8af21136b8
Monica Barboza <145809292+MonBoza@users.noreply.github.com>
2023-10-24 13:30:51 -0500

    updated JS and TDD

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,41 @@
 //Business Logic
 
-function wordCounter(text){
-    let wordCount = 0;
-    const textArray = text.split(" ");
-    textArray.forEach(function(word){
-        wordCount++;
-});
-return wordCount;
-}
+// function wordCounter(text){
+    // let wordCount = 0;
+    // const textArray = text.split(" ");
+    // textArray.forEach(function(word){
+        // wordCount++;
+// });
+// return wordCount;
+// }
 function wordCounter(text){
     if (text.trim().length === 0) {
-        return 0;
-    }
+         return 0;
+     }
     let wordCount = 0;
     const textArray = text.split(" ");
-    textArray.forEach(function(word){
+     textArray.forEach(function(element){
         if (!Number(element)){
         wordCount++;  
-     }      
+      }     
+
+     });
+    return wordCount;
+ }
+
+function numberOfOccurrencesInText(word, text) {
+    const textArray = text.split(" ");
+    let wordCount = 0;
+    textArray.forEach(function(element){
+    if (element.toLowerCase().includes(word.toLowerCase())) {
+        wordCount++
+     }
     });
     return wordCount;
+}
+function includesRarestLetter(word){
+    if (word.toLowerCae().includes("q")) {
+        return true 
+    }
+    return false;
 }
