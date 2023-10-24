6c5df7654646a52962463fcfdd19cea6df43cd8c
Monica Barboza <145809292+MonBoza@users.noreply.github.com>
2023-10-24 17:48:43 -0500

    update UI functions

--- README.md
+++ README.md
@@ -24,7 +24,7 @@ Test: "It should not count numbers as words."
 Code: wordCounter("hi there 77 19");
 Expected Output: 2
 
-Decribe : numberOfOccurrencesInText()
+Describe : numberOfOccurrencesInText()
 
 Test: "It should return 0 occurrences of a word for an empty string."
 Code:
@@ -61,3 +61,39 @@ const word = "Red";
 numberOfOccurrencesInText(word, text);
 Expected Output: 3
 
+Test: "If an empty string is passed in as a word, it should return 0."
+Code:
+const word = "";
+const text = "red RED Red!";
+numberOfOccurrencesInText(word, text);
+Expected Output: 0
+
+Describe: boldPassage()
+
+Test: "It should return null if no word or text is entered."
+Code:
+const text = "";
+const word = "";
+boldPassage(word, text);
+Expected Output: null
+
+Test: "It should return a non-matching word in a p tag."
+Code:
+const word ="hello";
+const text = "yo";
+boldPassage(word, text);
+Expected Output: <p>yo<p>
+
+Test: "Its Should return a matching word in a strong tag."
+Code:
+const word = "hello";
+const text = "hello;
+boldPassage(word, text);
+Expected Output: <p><strong>hello</strong></p>
+
+Test: "It should wrap words that match in strong tags but not words that don't."
+Code:
+const word = "hello";
+const text = "hello there";
+boldPassage(word, text);
+Expected Output: <p><strong>hello</strong> there</p>
--- js/scripts.js
+++ js/scripts.js
@@ -8,34 +8,105 @@
 // });
 // return wordCount;
 // }
-function wordCounter(text){
-    if (text.trim().length === 0) {
-         return 0;
-     }
+
+// Business Logic
+
+// utility logic
+function isEmpty(testString) {
+    return (testString.trim().length ===0);
+}
+function wordCounter(text) {
+    if (isEmpty(text)){
+      return 0;
+    }
     let wordCount = 0;
     const textArray = text.split(" ");
-     textArray.forEach(function(element){
-        if (!Number(element)){
-        wordCount++;  
-      }     
-
-     });
-    return wordCount;
- }
-
-function numberOfOccurrencesInText(word, text) {
-    const textArray = text.split(" ");
-    let wordCount = 0;
-    textArray.forEach(function(element){
-    if (element.toLowerCase().includes(word.toLowerCase())) {
-        wordCount++
-     }
+    textArray.forEach(function(element) {
+      if (!Number(element)) {
+        wordCount++;
+      }
     });
     return wordCount;
-}
-function includesRarestLetter(word){
-    if (word.toLowerCae().includes("q")) {
-        return true 
+  }
+  
+  function numberOfOccurrencesInText(word, text) {
+    if (isEmpty(word)) {
+        return 0;
     }
-    return false;
+    const textArray = text.split(" ");
+    let wordCount = 0;
+    textArray.forEach(function(element) {
+      if (element.toLowerCase().includes(word.toLowerCase())) {
+        wordCount++;
+      }
+    });
+    return wordCount;
+  }
+  function boldPassage(word, text) {
+    if (isEmpty(word) || isEmpty(text)) {
+        return null;
+    }
+  
+    function boldPassage(word, text) {
+        if ((text.trim().length === 0) || (word.trim().length === 0)) {
+          return null;
+        }
+        const p = document.createElement("p");
+        let textArray = text.split(" ");
+        textArray.forEach(function(element, index) {
+          if (word === element) {
+            const bold = document.createElement("strong");
+            bold.append(element);
+            p.append(bold);
+          } else {
+            p.append(element);
+          }
+          if (index !== (textArray.length - 1)) {
+            p.append(" ");
+          }
+        });
+        return p;
+      }
+  // UI Logic
+//function includesRarestLetter(word){
+    //if (word.toLowerCae().includes("q")) {
+    //    return true 
+   // }
+   // return false;
+//}
+// omit bad words :(
+// function omitBadWords(word, text) {
+
+ //   const textArray = text.split(" ");
+   // const filteredWords = textArray.filter(word => !word.includes(word));
+    //const result = filteredWords.join(" ");
+    //return result;
+
+//}
+
+//UI LOGIC
+ 
+// UI Logic
+
+// UI Logic
+
+function handleFormSubmission() {
+    event.preventDefault();
+    const passage = document.getElementById("text-passage").value;
+    const word = document.getElementById("word").value;
+    const wordCount = wordCounter(passage);
+    const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+    document.getElementById("total-count").innerText = wordCount;
+    document.getElementById("selected-count").innerText = occurrencesOfWord;
+    let boldedPassage = boldPassage(word, passage);
+  if (boldedPassage) {
+    document.querySelector("div#bolded-passage").append(boldedPassage);
+  } else {
+    document.querySelector("div#bolded-passage").innerText = null;
+  }
 }
+  }
+  
+  window.addEventListener("load", function() {
+    document.querySelector("form#word-counter").addEventListener("submit", handleFormSubmission);
+  });
