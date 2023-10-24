25a35b7bb07ff26bcd4225aeb4f743ecc5a9882b
Monica Barboza <145809292+MonBoza@users.noreply.github.com>
2023-10-24 12:28:06 -0500

    should correct the 5thtes

--- js/scripts.js
+++ js/scripts.js
@@ -15,7 +15,9 @@ function wordCounter(text){
     let wordCount = 0;
     const textArray = text.split(" ");
     textArray.forEach(function(word){
-        wordCount++;        
+        if (!Number(element)){
+        wordCount++;  
+     }      
     });
     return wordCount;
 }
